6628515456f28c7fa8cb5358b65338c0a98a5691
TeaInMud <mytotallycoolgmail@gmail.com>
2026-07-03 14:44:32 -0500

    Add docstring and version to __init__.py
    Added module docstring and versioning information.

--- tea_transpiler/__init__.py
+++ tea_transpiler/__init__.py
@@ -1,1 +1,10 @@
+"""
+Tea Language Transpiler
+Converts Tea source code to Python
+"""
 
+from .transpiler import transpile
+from .compiler import compile_tea, run_tea
+
+__version__ = "1.0.0"
+__all__ = ["transpile", "compile_tea", "run_tea"]
